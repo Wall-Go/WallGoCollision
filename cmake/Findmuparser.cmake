9387bc919f5bffe5007e3807c78e38efa56aed50
Lauri Niemi <lauri.b.niemi@helsinki.fi>
2023-10-09 18:27:52 +0300

    cmake files for finding/fetching muparser library

--- cmake/Findmuparser.cmake
+++ cmake/Findmuparser.cmake
@@ -5,6 +5,7 @@
 find_package(PkgConfig)
 pkg_check_modules(PC_MUPARSER QUIET muparser)
 
+
 find_path(MUPARSER_INCLUDE_DIR
 	NAMES muParser.h
 	PATHS ${PC_MUPARSER_INCLUDE_DIRS}
@@ -18,40 +19,50 @@ find_library(MUPARSER_LIBRARY
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(muparser
-  FOUND_VAR MUPARSER_FOUND
-  REQUIRED_VARS
-    MUPARSER_LIBRARY
-    MUPARSER_INCLUDE_DIR
-  VERSION_VAR MUPARSER_VERSION
+    FOUND_VAR MUPARSER_FOUND
+    REQUIRED_VARS
+        MUPARSER_LIBRARY
+        MUPARSER_INCLUDE_DIR
+    VERSION_VAR MUPARSER_VERSION
 )
 
 
 if(MUPARSER_FOUND)
 
+    if (NOT TARGET muparser::muparser)
+        ## define imported target
+        add_library(muparser::muparser UNKNOWN IMPORTED)
+        
+        set_target_properties(muparser::muparser
+            PROPERTIES IMPORTED_LOCATION ${MUPARSER_LIBRARY}
+            INTERFACE_INCLUDE_DIRECTORIES ${MUPARSER_INCLUDE_DIR}
+        )
+
+    endif()
+
 	set(MUPARSER_LIBRARIES ${MUPARSER_LIBRARY})
 	set(MUPARSER_INCLUDE_DIRS ${MUPARSER_INCLUDE_DIR})
 	set(MUPARSER_DEFINITIONS ${PC_MUPARSER_CFLAGS_OTHER})
-	
+
 else()
-	## PkgConfig was not able to find the package so it's probably not installed
 
-	## Let's try to fetch it
-	include(FetchContent)
-	Set(FETCHCONTENT_QUIET FALSE) ## Show what the fetching is doing
-	set(FETCHCONTENT_UPDATES_DISCONNECTED ON)
+    #package not found, so most likely not installed. Let's try fetching it
+    include(FetchContent)
+    Set(FETCHCONTENT_QUIET FALSE) ## Show what the fetching is doing
+    set(FETCHCONTENT_UPDATES_DISCONNECTED ON)
 
-	FetchContent_Declare(
-	  muparser
-	  GIT_REPOSITORY https://github.com/beltoforion/muparser.git
-	  GIT_TAG        v2.3.4
-	)
-	
-	## This is not sufficient - need to manually specify build/config commands...
+    FetchContent_Declare(
+        muparser
+        GIT_REPOSITORY https://github.com/beltoforion/muparser.git
+        GIT_TAG        v2.3.4
+        FIND_PACKAGE_ARGS NAMES MUPARSER
+    )
 
-	FetchContent_GetProperties(muparser)
-	if (NOT muparser_POPULATED)
-		FetchContent_Populate(muparser)
-	endif()
+    FetchContent_MakeAvailable(muparser)
 
-	
 endif()
+
+
+
+
+
