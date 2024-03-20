aeb5ab0b84ca97d45d2c59d5bef62063efc56778
Lauri Niemi <lauri.b.niemi@helsinki.fi>
2024-03-20 20:34:29 +0200

    refactoring matrix element parsing

--- src/MatrixElement.cpp
+++ src/MatrixElement.cpp
@@ -11,12 +11,19 @@
 namespace wallgo
 {
 
-MatrixElement::MatrixElement() {
-
+MatrixElement::MatrixElement() 
+{
     parser = new mu::Parser();
     s_internal = 0; t_internal = 0; u_internal = 0;
 
-    parser->SetExpr("0"); 
+    parser->SetExpr("0");
+
+    parser->DefineVar("s", &s_internal);
+    parser->DefineVar("t", &t_internal);
+    parser->DefineVar("u", &u_internal);
+
+    // To allow variable names like msq[2] we need to add [] to parser's character list
+    parser->DefineNameChars("0123456789_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ[]");
 }
 
 MatrixElement::~MatrixElement()
@@ -24,10 +31,11 @@ MatrixElement::~MatrixElement()
     delete parser;
 }
 
+
 MatrixElement::MatrixElement(const MatrixElement& other)
 {
-    couplings_internal = other.couplings_internal;
-    msq_internal = other.msq_internal;
+    parametersInternal = other.parametersInternal;
+    parametersInternal = other.parametersInternal;
     expression = other.getExpression();
     parser = new mu::Parser();
     initParser(couplings_internal, msq_internal);
@@ -48,31 +56,9 @@ void MatrixElement::operator=(const MatrixElement &other)
 
 void MatrixElement::initParser(const std::vector<double> &couplings, const std::vector<double> &massSquares)
 {
-    parser->DefineVar("s", &s_internal);
-    parser->DefineVar("t", &t_internal);
-    parser->DefineVar("u", &u_internal);
-
     setConstants(couplings, massSquares);
 
-    // Couplings are c[i], mass-squares are msq[i].
-    // To allow variable names like msq[2] we need to add [] to parser's character list
-    parser->DefineNameChars("0123456789_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ[]");
-
-    try {
-        for (uint i = 0; i < couplings_internal.size(); ++i) {
-            std::string coupling_str = "c[" + std::to_string(i) + "]";
-            parser->DefineVar(coupling_str, &couplings_internal[i]);
-        }
-
-        for (uint i = 0; i < msq_internal.size(); ++i) {
-            std::string coupling_str = "msq[" + std::to_string(i) + "]";
-            parser->DefineVar(coupling_str, &msq_internal[i]);
-        }
-
-    } catch (mu::Parser::exception_type &parserException) {
-        std::cerr << "=== Error when initializing symbols. Parser threw error: \n"; 
-        std::cerr << parserException.GetMsg() << std::endl;
-    }
+    
 }
 
 void MatrixElement::setExpression(const std::string &expressionIn)
@@ -84,14 +70,18 @@ void MatrixElement::setExpression(const std::string &expressionIn)
     testExpression();
 }
 
+void MatrixElement::initParser(const std::map<std::string, double> &parameters, const std::map<std::string, double> &massSquares)
+{
+}
+
 void MatrixElement::setConstants(const std::vector<double> &couplings, const std::vector<double> &massSquares)
 {
     couplings_internal = couplings;
     msq_internal = massSquares;
 }
 
-double MatrixElement::evaluate(double s, double t, double u) {
-
+double MatrixElement::evaluate(double s, double t, double u) 
+{
     s_internal = s;
     t_internal = t;
     u_internal = u;
@@ -99,8 +89,21 @@ double MatrixElement::evaluate(double s, double t, double u) {
     return parser->Eval();
 }
 
-void MatrixElement::testExpression() {
+void MatrixElement::defineSymbol(const std::string &symbol)
+{
+    try
+    {
+        parser->DefineVar(symbol, )
+    }
+    catch (mu::Parser::exception_type &parserException) 
+    {
+        std::cerr << "=== Error when initializing symbol '" << symbol << "'. Parser threw error: \n"; 
+        std::cerr << parserException.GetMsg() << std::endl;
+    }
+}
 
+void MatrixElement::testExpression() 
+{
     // try evaluate at some random values
     try {
         evaluate(-4.2, 2.9, 0);
--- src/include/WallGoCollision/MatrixElement.h
+++ src/include/WallGoCollision/MatrixElement.h
@@ -10,6 +10,12 @@
 namespace wallgo
 {
 
+/* Class MatrixElement - Describes a single symbolic matrix element (squared). Uses muparser for handling symbolic expressions.
+Logic here is that the default constructor just constructs a dummy element with expression equal to 0, and does not define any symbols. 
+Calling initParser(parameters) defines the Mandelstam (s,t,u) as free symbols that are set later during integration, 
+and also defines a symbol for each key in the 'parameters' std::map.
+The parameters map should contain a definition for each symbol that appears in the matrix element expression.
+Calling setParameters(parameters) fixes numerical values for all these symbols. Currently this setting happens automatically in initParser. */
 class MatrixElement {
 
 public:
@@ -25,15 +31,17 @@ public:
 
     std::string getExpression() const { return expression; }
 
-    /* Initialize the math expression parser. This defines symbolic variables s,t,u ; couplings[i] and msq[i]. 
+    /* Initialize the math expression parser. This defines symbolic variables (s,t,u) and a symbol for each string in 'parameters'.
     Needs to be called before attempting evaluation */
-    void initParser(const std::vector<double>& couplings, const std::vector<double>& massSquares);
+    void initParser(const std::map<std::string, double>& parameters);
 
-    // This fixes couplings and masses, caching them internally
-    void setConstants(const std::vector<double>& couplings, const std::vector<double>& massSquares);
+    // This gives numerical values to all our internal symbols except for (s,t,u), as defined in initParser.
+    void setParameters(const std::map<std::string, double>& parameters);
 
     // Evaluate the matrix element at s,t,u, using cached couplings and masses
     double evaluate(double s, double t, double u);
+
+    void defineSymbol(const std::string& symbol);
 
 
     /* Math expression parser. Might as well make it a pointer - had issues with this breaking when passing MatrixElements around with default constructors */
@@ -44,8 +52,8 @@ private:
 
     double s_internal, t_internal, u_internal;
 
-    std::vector<double> couplings_internal;
-    std::vector<double> msq_internal;
+    // Internal map of model-specific symbols that are not (s,t,u), and their current values
+    std::map<std::string, double> parametersInternal;
 
     // Tests that our expression is valid and can be evaluated by the parser
     void testExpression();
