d42ae86fc6fd4b0c2d3aa96f91c856de041be41e
Lauri Niemi <lauri.b.niemi@helsinki.fi>
2024-08-29 20:25:16 +0300

    Fixed OMP compile definitions, now they correctly propagate to the python module

--- src/include/WallGo/EnvironmentMacros.h
+++ src/include/WallGo/EnvironmentMacros.h
@@ -30,15 +30,14 @@ AND manually mark appropriate class members for exporting (not done). */
 #endif
 
 // NB: For MSVC the default OMP version is only 2.X, so some newer features may not be available
-#if defined WITH_OMP && _OPENMP >= 200805 // OMP >= 3.0
+#if WITH_OMP && _OPENMP >= 200805 // OMP >= 3.0
     #define WG_OMP_SUPPORTS_COLLAPSE 1
 #else
     #define WG_OMP_SUPPORTS_COLLAPSE 0
 #endif
 
-
 // OMP atomic read/write is 3.1 feature. If not available, we force a critical section
-#if !defined WITH_OMP
+#if !WITH_OMP
     #define WG_PRAGMA_OMP_ATOMIC_READ
     #define WG_PRAGMA_OMP_ATOMIC_WRITE
 
@@ -53,7 +52,7 @@ AND manually mark appropriate class members for exporting (not done). */
 #endif
 
 
-#if defined WITH_OMP && _MSC_VER
+#if WITH_OMP && _MSC_VER
     /* VS implementation of OMP doesn't allow threadprivate on extern variables.
     See https://stackoverflow.com/questions/12560243/using-threadprivate-directive-in-visual-studio */
     #define WG_THREADPRIVATE_EXTERN_VARIABLE(Type, varName) extern __declspec(thread) Type varName;
